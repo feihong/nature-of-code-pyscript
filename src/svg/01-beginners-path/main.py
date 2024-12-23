2c27dcea4704d0941eb0afdd2775bd574f3c19b9
Feihong Hsu <feihong.hsu@gmail.com>
2024-12-22 23:30:05 -0600

    Refactor svg programs for clarity

--- src/svg/01-beginners-path/main.py
+++ src/svg/01-beginners-path/main.py
@@ -11,24 +11,29 @@ def hilma():
     circle(r=50, cy=250, cx=250, fill='#d6a946'),
 ])
 
-def ellipse_i(i, hue, rotation):
+def ellipses(hue, rotation, iterations):
     center = 500
-    rotation = rotation + (i * 2)
-    neg_pos = (1 if hue < 180 else -1)
-    hue = hue + neg_pos * (i * 3)
-    return ellipse(cx=center, cy=center, rx=100 + (i * 3), ry=300 + (i * 2), fill='none',
-        stroke=f'hsla({hue}, 80%, 80%, 0.6)', transform=f'rotate({rotation} {center} {center})')
+    neg_pos = 1 if hue < 180 else -1
 
-def loops():
+    for i in range(iterations):
+        yield ellipse(
+            cx=center,
+            cy=center,
+            rx=100 + (i * 3),
+            ry=300 + (i * 2),
+            fill='none',
+            stroke=f'hsla({hue + neg_pos * (i * 3)}, 80%, 80%, 0.6)',
+            transform=f'rotate({rotation + (i * 2)} {center} {center})')
+
+def neon_loops():
     size = window.innerWidth if window.innerWidth < window.innerHeight else window.innerHeight
-
     hue = random.randint(0, 360)
     rotation = random.randint(-180, 180)
     iterations = random.randint(10, 100)
 
     return svg(width=size, height=size, viewBox='0 0 1000 1000', children=[
         rect(x=0, y=0, width=1000, height=1000, fill='#181818'),
-        *(ellipse_i(i, hue, rotation) for i in range(iterations))
+        *ellipses(hue, rotation, iterations),
     ])
 
 sketch = page.find('#sketch')[0]
@@ -40,13 +45,13 @@ sketch.append(
        div('(press any key to regenerate)'),
        style={'display': 'flex', 'gap': '8px', 'align-items': 'baseline'}
     ),
-    div(loops(), id='loops'),
+    div(neon_loops(), id='loops'),
 )
 
 def regenerate():
     dv = page.find('#loops')[0]
     dv.innerHTML = ''
-    dv.append(loops())
+    dv.append(neon_loops())
 
 @when('keypress', 'html')
 def keypress(_event): regenerate()
--- src/svg/02-programming-primer/main.py
+++ src/svg/02-programming-primer/main.py
@@ -7,20 +7,19 @@ from sv import svg, rect, circle, g
 def circles():
     steps = ['00', '33', '66', '99', 'CC', 'FF']
     web_safe_colors = (f'#'+r+g+b for r,g,b in itertools.product(steps, repeat=3))
+    count = len(steps) ** 3
 
-    angle = 0
-    radius = 5
-    increment = (math.pi * 12) / (len(steps) ** 3)
+    increment = math.pi * 12 / count
 
     for i, web_safe_color in enumerate(web_safe_colors):
         angle = increment * i
+        radius = 5 + 2 * i
         yield circle(
             cx=math.cos(angle) * radius,
             cy=math.sin(angle) * radius,
             r=1 + (i / 20.),
             transform='translate(500, 500)',
             fill=web_safe_color)
-        radius += 2
 
 size = window.innerWidth if window.innerWidth < window.innerHeight else window.innerHeight
 
